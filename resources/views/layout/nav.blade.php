b6f18a35980eac61377d6f5d22fc99489ad69ef1
emman <emmanueltuboran.141032@gmail.com>
2024-05-09 15:37:08 +0800

    wew

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -48,6 +48,11 @@
         @endphp
         <li class="">
             <a href="{{ route('users.show') }}" class="hover:text-spcmain flex flex-row items-center">
+                create
+            </a>
+        </li>
+        <li class="">
+            <a href="{{ route('users.show') }}" class="hover:text-spcmain flex flex-row items-center">
                 <img class="w-12 mr-2" src="{{ asset($collegelogo) }}" alt="{{ $college }} Logo" class="logo" />
                 <p>{{ Auth::user()->name }} Profile</p>
             </a>
