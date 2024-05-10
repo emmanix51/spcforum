e8be7505556c15b42bc68b40ee76b06d1acc253d
emman <emmanueltuboran.141032@gmail.com>
2024-05-10 15:31:56 +0800

    changed footer

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -31,10 +31,31 @@
 
   @yield('content')
 
-  <footer
-    class="bottom-0 left-0 w-full flex items-center justify-start font-bold bg-spcmain text-white h-6 mt-24 opacity-90 md:justify-center text-base">
-    <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
+
+
+  <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="{{ route('dashboard') }}"
+          class="hover:underline">Spcforum</a>. All Rights Reserved.
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline">Contact</a>
+        </li>
+      </ul>
+    </div>
   </footer>
+
+
 </body>
 
 </html>
